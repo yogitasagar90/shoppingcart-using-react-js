2f46f4f121def06934a85e62af8128c5d63ac76d
Yogita Kale <yogitasagar7557@gmail.com>
2023-07-12 10:06:17 +0530

    workon shoppingcart using react js

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
+
+const data ={
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'Grapes',
+            desc:'',
+            price: 60,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Apple',
+            desc:'',
+            price: 140,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Orange',
+            desc:'',
+            price: 65,
+        },
+    ]
+};
+
+export default data;
